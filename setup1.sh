029f61900fda8265259334dd266ba5e62060280a
root <root@workstation.lab.example.com>
2022-01-25 05:07:12 -0500

    25012022

--- setup1.sh
+++ setup1.sh
@@ -3,7 +3,8 @@ cd /root
 git clone https://github.com/anandpavithran/operator-yaml
 wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
 tar -C /usr/local -xzf /root/go1.17.6.linux-amd64.tar.gz
-echo "PATH=$PATH:/usr/local/go/bin" >> /root/.bash_profile
+rm -rf /root/go*
+echo "PATH=\$PATH:/usr/local/go/bin" >> /root/.bash_profile
 echo "GOPATH=/usr/local/go" >> /root/.bash_profile
 source /root/.bash_profile
 cd /root 
@@ -19,3 +20,4 @@ cd /root
 operator-sdk new web --kind=Web --api-version=web.operator.io/v1 --type=ansible
 cd /root/web
 cp /root/operator-yaml/main.yml /root/web/roles/web/tasks/main.yml 
+kill -9 $(pgrep bash)
--- setup2.sh
+++ setup2.sh
@@ -6,9 +6,10 @@ read -r -s var2
 podman login quay.io -u $var1 -p $var2
 if [ $? -eq 0 ]
 then
+cd /root/web;
 operator-sdk build quay.io/$var1/web --image-builder podman 
 podman push quay.io/$var1/web:latest
-
+sed -i 's/REPLACE_IMAGE/quay.io\/$var1\/web/g' /root/web/deploy/operator.yaml
 else
 echo "Something goes wrong"
 fi
