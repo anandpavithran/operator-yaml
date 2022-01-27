ae9fe8a67a86f281ee46c8d028e03193b439e40c
Audit <audit.gmail.com>
2022-01-27 12:15:52 +0530

    27012022

---
+++ lab5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##################
+RUN AS ROOT USER
+##################
+echo Enter your name:
+read var1
+oc login -u admin -p redhat https://api.ocp4.example.com:6443
+oc new-project weboperator-$var1
+cd /root/web/deploy/
+oc create -f /root/web/deploy/service_account.yaml
+oc adm policy add-cluster-role-to-user cluster-admin -z web
+oc create -f /root/web/deploy/crds/web.operator.io_webs_crd.yaml
+oc create -f /root/web/deploy/operator.yaml
+
